8632cb2733a61e5643f8af68e0cfa06e28e1df85
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-08-29 09:09:06 +0300

    Create a function to fetch and display the movie list

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const container = document.querySelector('.container');
+
 async function fetchMovies(query) {
     const movieData = await fetch("https://ghibliapi.herokuapp.com/films");
     const data = movieData.json();
-    console.log(data)
     return data;
     
 }
 
-fetchMovies();
+async function fetchMovieAndDisplay(query) {
+    const movies = await fetchMovies(query);
+    console.log(movies)
+
+}
+
+fetchMovieAndDisplay();
